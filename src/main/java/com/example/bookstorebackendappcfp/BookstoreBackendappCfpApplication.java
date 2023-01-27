01526a3929985cf32ef1bebe5020cc496bcfd017
ilis137 <iliyasshahapure1@gmail.com>
2023-01-27 18:15:41 +0530

    initial commit

---
+++ src/main/java/com/example/bookstorebackendappcfp/BookstoreBackendappCfpApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstorebackendappcfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreBackendappCfpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreBackendappCfpApplication.class, args);
+    }
+
+}
